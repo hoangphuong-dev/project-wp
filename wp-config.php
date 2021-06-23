885a7b544a03772a6624c4774bb1504d2500c138
Phương Hoàng <hoangphuong2311bkhn@gmail.com>
2021-06-23 17:29:08 +0700

    add file huong dan

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'bkd01k11' );
 define( 'DB_USER', 'bkd01k11_user' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'fQqUgDRprpACUwUs' );
+define( 'DB_PASSWORD', 'e8[D.gg.5ElUoKn1' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
